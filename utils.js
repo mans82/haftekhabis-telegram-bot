e34e366daa5ca01d45a0f2fca9915ebae34fbfde
AmirHosein <cando428@gmail.com>
2020-07-09 23:28:03 +0430

    feat: Implement Player class

--- utils.js
+++ utils.js
@@ -1,4 +1,18 @@
-class CardDeck {
+class Base {
+    constructor(){}
+
+    _isValidCard(card){
+        if (typeof card === 'string'){
+            if (this._cardSuites.includes(card[0]) && this._cardNumbers.includes(card[1])){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+class CardDeck extends Base{
     constructor(){
         this._cards = [];
         this._cardNumbers = '1234567890JKQ';
@@ -12,16 +26,6 @@ class CardDeck {
         }
 
         this._topCardIndex = Math.floor(Math.random() * this._cards);
-    }
-
-    _isValidCard(card){
-        if (typeof card === 'string'){
-            if (this._cardSuites.includes(card[0]) && this._cardNumbers.includes(card[1])){
-                return true;
-            }
-        }
-
-        return false;
     }
 
     getCard(){
@@ -46,5 +50,41 @@ class CardDeck {
 
     getTopCard(){
         return this._cards[this._topCardIndex];
+    }
+}
+
+class Player extends Base{
+    constructor(name, chatId, messageId){
+        this.name = name;
+        this.chatId = chatId;
+        this.messageId = messageId;
+        this.ready = false;
+        this.rank = -1; // -1 means this player has not finished playing. otherwise it is the rank of the player.
+        this._cards = [];
+    }
+
+    get cards(){
+        return [...this._cards]; // copy of cards
+    }
+
+    put_card(card){
+        if (!this._isValidCard(card)){
+            throw 'Invalid card';
+        }
+        this._cards.push(card)
+    }
+
+    take_card(card){
+        if (!this._isValidCard(card)){
+            throw 'Invalid card';
+        }
+        if (!this._cards.includes(card)){
+            throw 'Card does not exist';
+        }
+        this._cards.splice(card);
+    }
+
+    has_no_card(){
+        return this._cards.length == 0;
     }
 }

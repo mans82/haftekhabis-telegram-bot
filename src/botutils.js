112b498cbad47f807a818df041a0d81aa1f5482f
AmirHosein <cando428@gmail.com>
2020-08-25 19:51:17 +0430

    feat: Implement event emission in RoomManager

--- src/botutils.js
+++ src/botutils.js
@@ -1,26 +1,46 @@
 const utils = require('./utils');
 const EventEmitter = require('events').EventEmitter;
 
-class RoomManager {
+class RoomManager extends EventEmitter{
     constructor(){
+        super();
         this._rooms = {};
-        this._eventemitter = new EventEmitter();
-        this.on = this._eventemitter.on;
+        // this._eventemitter = new EventEmitter();
     }
 
-    createRoom(creatorPlayer, creatorMessageId, name = 'New room'){
-        let creatorChatId = creatorPlayer.chatId;
-        let roomObj = new utils.GameRoom();
-        roomObj.addPlayer(creatorPlayer);
-        let newRoom = {
+    createRoom(creatorPlayer, creatorMessageId, name = 'New Room'){
+        const creatorChatId = creatorPlayer.chatId;
+        const roomObj = new utils.GameRoom();
+        const newRoom = {
             name: name,
             roomObj: roomObj,
             messageInfo: [
                 {creatorChatId: creatorMessageId}
             ]
         }
+        roomObj.on('new-player-added', (player) => {
+            newRoom.messageInfo[player.chatId] = player.messageId;
+            this.emit('new-player-added', roomObj, player);
+        });
+        roomObj.on('game-started', () => {
+            this.emit('game-started', roomObj);
+        });
+        roomObj.on('turn-changed', () => {
+            this.emit('turn-changed', roomObj);
+        });
+        roomObj.on('player-to-fine', (finerPlayer) => {
+            this.emit('player-to-fine', roomObj, finerPlayer);
+        });
+        roomObj.on('game-finished', () => {
+            this.emit('game-finished', roomObj);
+        });
+        roomObj.addPlayer(creatorPlayer);
         this._rooms[creatorChatId] = newRoom;
-        this._eventemitter.emit('room-created'); // TODO provide arguments
+        this.emit('new-room-created', name, creatorChatId, creatorMessageId);
+    }
+
+    getRoomByCreatorChatId(chatId){
+        return this._rooms[chatId];
     }
 }
 
